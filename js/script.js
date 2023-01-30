eff9ee7d0bdb00cd379946a2541d8ec49311b2c3
din-islam-sh1 <dinislamshowrov.2001@gmail.com>
2023-01-31 00:09:42 +0600

    ...

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Hero Part
+new VenoBox({
+    selector: '.open-the-vdo',
+    navigation: true,
+    autoplay: false,
+    bgcolor: '#720a0a',
+    spinner: 'wave',
+    spinColor: '#ddd',
+    share: true
+});
